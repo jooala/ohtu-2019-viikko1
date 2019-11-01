3c7cf569bf7d05e3f1271c4bcab1a575808123cd
Joonas Alanenpää <joonas.alanenpaa@gmail.com>
2019-11-01 03:08:27 +0200

    varasto luokan testit 100%

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -24,7 +24,12 @@ public class VarastoTest {
     public void konstruktoriLuoTyhjanVaraston() {
         assertEquals(0, varasto.getSaldo(), vertailuTarkkuus);
     }
-
+    @Test
+    public void varastonArvoOnNollaJosVääräArvo() {
+        Varasto varasto2 = new Varasto(-2);
+        assertEquals(0.0, varasto2.getTilavuus(), vertailuTarkkuus);
+        
+    }
     @Test
     public void uudellaVarastollaOikeaTilavuus() {
         assertEquals(10, varasto.getTilavuus(), vertailuTarkkuus);
@@ -45,7 +50,39 @@ public class VarastoTest {
         // vapaata tilaa pitäisi vielä olla tilavuus-lisättävä määrä eli 2
         assertEquals(2, varasto.paljonkoMahtuu(), vertailuTarkkuus);
     }
-
+    @Test
+    public void lisaysVarastoonVaarillaArvoilla() {
+        varasto.lisaaVarastoon(-5);
+        assertEquals(10, varasto.paljonkoMahtuu(), vertailuTarkkuus);
+        varasto.lisaaVarastoon(15);
+        assertEquals(10, varasto.getSaldo(), vertailuTarkkuus);
+    }
+    @Test
+    public void otaVarastonVaarienArvojenTestaus() {
+        varasto.lisaaVarastoon(8);
+        varasto.otaVarastosta(-5);
+        assertEquals(8, varasto.getSaldo(), vertailuTarkkuus);
+        varasto.otaVarastosta(10);
+        assertEquals(0, varasto.getSaldo(), vertailuTarkkuus);
+    }
+    @Test
+    public void merkkijonoesitysToimii() {
+        String varastoString = "saldo = 0.0, vielä tilaa 10.0";
+        assertEquals(varasto.toString(), varastoString);
+    }
+    @Test
+    public void varastoMissaAlkuSaldoMyosToimii() {
+        Varasto varastoAlku = new Varasto(15, 5);
+        assertEquals(15, varastoAlku.getTilavuus(), vertailuTarkkuus);
+        assertEquals(5, varastoAlku.getSaldo(), vertailuTarkkuus);
+        Varasto varastoAlkuVäärin = new Varasto(-3, -5);
+        assertEquals(0, varastoAlkuVäärin.getTilavuus(), vertailuTarkkuus);
+        assertEquals(0, varastoAlkuVäärin.getSaldo(), vertailuTarkkuus);
+        Varasto varastoAlkuTäynnä = new Varasto (7, 12);
+        assertEquals(7, varastoAlkuTäynnä.getTilavuus(), vertailuTarkkuus);
+        assertEquals(7, varastoAlkuTäynnä.getSaldo(), vertailuTarkkuus);
+        
+    }
     @Test
     public void ottaminenPalauttaaOikeanMaaran() {
         varasto.lisaaVarastoon(8);
